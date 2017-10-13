aa348dc804f539c05ff2a2a46a78f28888e03aa5
Jim Eli <31859133+JimEli@users.noreply.github.com>
2017-10-13 05:21:45 -0600

    Update main.c

--- pthreads_prime_numbers/main.c
+++ pthreads_prime_numbers/main.c
@@ -2,6 +2,7 @@
 Instructions for implementing pthreads on MS Visual Studio 2017:
 
 Download: http://cs.du.edu/~mitchell/pthreads_compiled.zip
+unzip archive.
 add: pthread.h, sched.h & semaphore.h to "C:\Program Files (x86)\Windows Kits\10\Include\10.0.15063.0\ucrt"
 add: pthreadVC2.lib to "C:\Program Files (x86)\Windows Kits\10\Lib\10.0.15063.0\ucrt\x86"
 add: pthreadVC2.dll to "C:\Windows"
